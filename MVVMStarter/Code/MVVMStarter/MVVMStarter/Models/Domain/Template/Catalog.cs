57ef37c14e6d766b6896a3c6e24c72060f13bdba
Per Storgaard Laursen <perl@easj.dk>
2017-05-01 15:23:21 +0200

    Updated Catalog Template (fixed DomainModel -> Catalog)

--- MVVMStarter/Code/MVVMStarter/MVVMStarter/Models/Domain/Template/Catalog.cs
+++ MVVMStarter/Code/MVVMStarter/MVVMStarter/Models/Domain/Template/Catalog.cs
@@ -14,14 +14,14 @@ namespace MVVMStarter.Models.Domain._REPLACEME_
     public class Catalog : CatalogBase<_REPLACEME_>
     {
         #region Model Singleton implementation
-        private static DomainModel _instance = null;
+        private static Catalog _instance = null;
 
-        public static DomainModel Instance
+        public static Catalog Instance
         {
             get
             {
                 if (_instance != null) return _instance;
-                _instance = new DomainModel();
+                _instance = new Catalog();
                 return _instance;
             }
         }
@@ -29,7 +29,7 @@ namespace MVVMStarter.Models.Domain._REPLACEME_
         /// <summary>
         /// Use a file-based persistent source
         /// </summary>
-        private DomainModel() : base(new CollectionBase<_REPLACEME_>(), new FileSourceBase<_REPLACEME_>())
+        private Catalog() : base(new CollectionBase<_REPLACEME_>(), new FileSourceBase<_REPLACEME_>())
         {
         }
         #endregion
